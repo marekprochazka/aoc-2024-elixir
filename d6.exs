bfd26f481aba3031af8e47425faa4951d40eaaef
Marek Procházka <m.prochazka2002@gmail.com>
2024-12-06 21:12:18 +0100

    d6 p1 (ugly...)

--- d6.exs
+++ d6.exs
@@ -24,8 +24,8 @@ defmodule IP do
 
 
     defp get_player_position(map) do
-        {x, y} = for {row, y} <- Enum.with_index(map), {tile, x} <- Enum.with_index(row), tile in [:player_north, :player_south, :player_west, :player_east], do: {x, y}
-        {x, y}
+        l = for {row, y} <- Enum.with_index(map), {tile, x} <- Enum.with_index(row), tile in [:player_north, :player_south, :player_west, :player_east], do: {x, y}
+        hd l
     end
 
     def parse_input(filename) do
@@ -33,7 +33,69 @@ defmodule IP do
         |> String.split("\n")
         |> Enum.map(&String.split(&1, "", trim: true))
         |> Enum.map(&Enum.map(&1, fn tile -> tile_to_atom(tile) end))
-        |> get_player_position
+        |> fn map -> {map, get_player_position(map)} end.()
     end
+
+    def get_tile_in_front(map, player_position) do
+        {x, y} = player_position
+        direction = map |> Enum.at(y) |> Enum.at(x)
+        {new_x, new_y} = get_new_xy(player_position, direction)
+        if xy_in_range?(map, new_x, new_y) do
+            map |> Enum.at(new_y) |> Enum.at(new_x)
+        else
+            :out_of_bounds
+        end
+    end
+
+    def get_next_move(map, player_position) do
+        tile_in_front = get_tile_in_front(map, player_position)
+        case tile_in_front do
+            v when v in [:empty, :visited] -> :move
+            :wall -> :turn
+            :out_of_bounds -> :stop
+        end
+    end
+
+    def get_new_xy(player_position, direction) do
+        {x, y} = player_position
+        case direction do
+            :player_north -> {x, y-1}
+            :player_south -> {x, y+1}
+            :player_west -> {x-1, y}
+            :player_east -> {x+1, y}
+        end
+    end
+
+    def xy_in_range?(map, x, y) do
+        x >= 0 && y >= 0 && y < Enum.count(map) && x < Enum.count(Enum.at(map, 0))
+    end
+
+    def move(map, player_position, :OK) do
+        {x, y} = player_position
+        direction = map |> Enum.at(y) |> Enum.at(x)
+        {new_x, new_y} = get_new_xy(player_position, direction)
+
+        next_move = get_next_move(map, player_position)
+
+        case next_move do
+            :stop -> List.replace_at(map, y, List.replace_at(Enum.at(map, y), x, :visited)) |> move(player_position, :OUT_OF_BOUNDS)
+            :turn -> List.replace_at(map, y, List.replace_at(Enum.at(map, y), x, next_direction(direction))) |> move({x,y}, :OK)
+            :move -> List.replace_at(map, y, List.replace_at(Enum.at(map, y), x, :visited)) |> (fn new_map -> List.replace_at(new_map, new_y, List.replace_at(Enum.at(new_map, new_y), new_x, direction)) end).() |> move({new_x, new_y}, :OK)
+        end
+    end
+
+    def move(map, _, :OUT_OF_BOUNDS) do
+        map
+    end
+
+    def sum_visited(map) do
+        map |> Enum.reduce(0, fn row, acc -> acc + Enum.count(Enum.filter(row, fn tile -> tile == :visited end)) end)
+    end
+
+    def main(filename) do
+        {map, player_position} = parse_input(filename)
+        move(map, player_position, :OK) |> sum_visited
+    end
+
 
 end
